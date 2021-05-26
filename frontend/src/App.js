d2b43c867f6f480c96c462428132f975add06d9e
Vitor Guedes <vittorbassdev@gmail.com>
2021-05-26 18:22:08 -0300

    Adicionando o Hook UseEffect Busca os repositorios ao carregar a pagina

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,40 @@
-// import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios'
 
 function App() {
+
+  // Hooks Utilizados
+
+  // -- useEffect
+  // -- useState
+
+    // VARIÁVEL PARA ARMAZENAR O VALOR DO INPUT DO FORM
+    // const [user, setUpdateUser] = useState()
+
+    const [projetcUser, updateProject] = useState({})
+    const [error, updateError] = useState()
+    
+  useEffect(() => {
+
+    try {
+      axios
+      //.get(`https://api.github.com/repos/VitorBassDev/angularJoaoPimentel`)
+      .get(`https://api.github.com/users/VitorBassDev/repos?page=3&per_page=4`,{
+    }).then(response =>{
+        updateProject(response.data);
+        console.log(response.data)
+      })
+    } catch (error) {
+      updateError(error.message)
+      console.log('Erro ao buscar repo')
+    }
+    
+  },[])
+
+  if(error){
+    return<h1> Deu erro: {error} </h1>
+  }
+
   return (
   <>
   <div classNameName="container">
@@ -17,10 +51,22 @@ function App() {
         </p>
 
         <div className="row p-2 justify-content-center mb-2 ">
-          <div className="col-md-5 ">
+          <div className="col-md-5 " onSubmit="">
             <form className="form-inline mt-2 mt-md-0">      
-              <input class="form-control mr-sm-2" type="text" placeholder="Nome do usuário"/>
-              <a className="btn btn-md btn-primary mt-3" href="../../components/navbar/" role="button">Buscar »</a>  
+              <input 
+                class="form-control mr-sm-2" 
+                type="text"
+                value={projetcUser}
+                placeholder="Nome do usuário"
+                // onChange={onChangeInput}
+                // onChange={ e => setUpdateUser(e.target.value)}
+                />
+              <button 
+                className="btn btn-md btn-primary mt-3" 
+                type="submit" 
+                >
+                  Buscar »
+              </button>  
             </form>
           </div>
         </div>
@@ -31,27 +77,11 @@ function App() {
     <div className="container">
 
       <div className="row">
-        <div className="col-md-3">
-          <h2>Título</h2>
+        <div className="col-md-6">
+          {/* <h2>Nome do Projeto: {projetc.data.svn_url}</h2> */}
           <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>      
         </div>
-        
-        <div className="col-md-3">
-          <h2>Título</h2>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
 
-        </div>
-        
-        <div className="col-md-3">
-          <h2>Título</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-        </div>
-
-        <div className="col-md-3">
-          <h2>Título</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-        </div>
-        <hr/>
       </div>
     </div>
       
