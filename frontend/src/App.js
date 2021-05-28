9b6ed6e76ba40100af7c5417e12b917de67a08af
Vitor Guedes <vittorbassdev@gmail.com>
2021-05-28 13:11:25 -0300

    Recetory to frontend Retorna os repositorios do usuario

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,53 +1,43 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios'
+import connectionApi from '../src/services/connectionGitHub'
 
 function App() {
 
-  // Hooks Utilizados
+  const [ projects, setProjects ] = useState([])
 
-  // -- useEffect
-  // -- useState
-
-    // VARIÁVEL PARA ARMAZENAR O VALOR DO INPUT DO FORM
-    // const [user, setUpdateUser] = useState()
-
-    const [projetcUser, updateProject] = useState({})
-    const [error, updateError] = useState()
-    
   useEffect(() => {
+    loadProjects()
+  }, [])    
 
-    try {
-      axios
-      //.get(`https://api.github.com/repos/VitorBassDev/angularJoaoPimentel`)
-      .get(`https://api.github.com/users/VitorBassDev/repos?page=3&per_page=4`,{
-    }).then(response =>{
-        updateProject(response.data);
-        console.log(response.data)
-      })
-    } catch (error) {
-      updateError(error.message)
-      console.log('Erro ao buscar repo')
+  async function loadProjects(){
+    try{
+      const response = await connectionApi.get(`usersGit`)
+      // const response = await axios.get(`https://api.github.com/users/VitorBassDev/repos`)
+      setProjects(response.data)
+      console.log(response.data)
+    } catch(err){
+      alert('It was not possible to find projects!')
     }
-    
-  },[])
-
-  if(error){
-    return<h1> Deu erro: {error} </h1>
   }
 
   return (
   <>
   <div classNameName="container">
-    <nav className="navbar navbar-dark bg-dark mb-4 text-center">
-      <a className="navbar-brand" href="google">Consumir API do GitHub</a>
+    <nav className="navbar navbar-dark bg-dark mb-4 ">
+      <p className=" h1 mx-auto"> 
+        <a className="navbar-brand" href="google">Consumir API do GitHub</a>
+      </p>
     </nav>
 
     <main role="main" className="container">
       <div className="jumbotron text-center">
         <h1>Consumindo a API do GitHub</h1>
         <p className="lead"> 
-          Por padrão, está página busca automáticamente os repositórios do usuário VittorBassDev,
-          para pesquisar outro usuário preenha este campo e clique em buscar.
+          Por padrão, está página busca automáticamente os repositórios do usuário VittorBassDev !
+        </p>
+        <p>
+          Para pesquisar outro usuário preenha o fomulário abaixo.
         </p>
 
         <div className="row p-2 justify-content-center mb-2 ">
@@ -56,10 +46,7 @@ function App() {
               <input 
                 class="form-control mr-sm-2" 
                 type="text"
-                value={projetcUser}
                 placeholder="Nome do usuário"
-                // onChange={onChangeInput}
-                // onChange={ e => setUpdateUser(e.target.value)}
                 />
               <button 
                 className="btn btn-md btn-primary mt-3" 
@@ -74,16 +61,25 @@ function App() {
       </div>
     </main>
 
+    { projects.length > 0 &&
+
     <div className="container">
+      {projects.map(project => (
 
       <div className="row">
         <div className="col-md-6">
-          {/* <h2>Nome do Projeto: {projetc.data.svn_url}</h2> */}
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>      
+          <h2>Nome do Projeto: </h2> 
+          <p>{project.name}</p>         
+          <p>
+            {project.language}
+          </p>
+          
         </div>
 
       </div>
+      ))}
     </div>
+    }
       
   </div>
   </>
@@ -91,10 +87,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
-
-
